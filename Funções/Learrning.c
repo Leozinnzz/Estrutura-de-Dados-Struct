a59e62dd2343121a8df632516a3be926f6dcdb2a
Leozinnzz <leozerathelegend@gmail.com>
2025-12-04 20:38:17 -0300

    Apenas exercicios para mehlorar minha logica

---
+++ Funções/Learrning.c
@@ -0,0 +1,48 @@
+/*
+============= Leozinzz =================
+============= CachyOS --%d/%m/%Y-- =================
+============= LI4ever ======================
+============= Made-by-Leozinzz-Desktop ===============
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct {
+	char origem[100];
+	char destino[100];
+	int tempo;
+	int distancia;
+}Rotas; 
+
+typedef struct{
+	int numRotas[100];
+	int cont;
+}dsRotas;
+
+
+void definirRotas(dsRotas *numR);
+void interface();
+Rotas registrar();
+
+
+int main() {
+	
+	
+}
+
+Rotas definirRotas(){
+	dsRotas numR;
+	numR.cont = 0;
+	return numR;
+}
+
+void interface(){
+	printf("")
+}
+
+
+
+
+
