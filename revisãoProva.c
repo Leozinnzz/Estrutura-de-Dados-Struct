2cd285b0ddfd16f89a5f282b9c06a78650f54969
Leozinnzz <leozerathelegend@gmail.com>
2025-12-10 13:07:48 -0300

    Revisao para a prova concluida

--- revisãoProva.c
+++ revisãoProva.c
@@ -11,41 +11,6 @@
 #include "input.h"
 
 
-//REVISÃO FINAL PARA A PROVA #02
-//---- SISTEMA ACADÊMICO -------
-
-//1 - CADASTRAR ALUNO
-	//MATRICULA(I: AUTOINCREMENTO) 
-	//NOME(S)
-
-//2 - CADASTRAR DISCIPLINA
-	//CODIGO(I: AUTOINCREMENTO)
-	//NOME(S)
-	//PERIODO(I)
-	//MATRICULAS(I[50])
-
-//3 - MATRICULAR ALUNO
-	//MATRICULAR UM ALUNO EM UMA DETERMINADA DISCIPLINA
-	//BUSCAR DISCIPLINA E ALUNO ATRAVÉS DE NOMES.
-	//SOMENTE 1 MATRICULA POR ALUNO/DISCIPLINA (VALIDAR!)
-
-//4 - CANCELAR MATRICULA (EXCLUIR MATRICULA)
-	//EXCLUIR A MATRICULA DE UM DETERMINADO ALUNO 
-	//EM UMA DISCIPLINA.
-
-//5 - FAZER CHAMADA
-	//SOLICITAR NOME DE DISCIPLINA.
-	//LISTAR (EM ORDEM ALFABETICA) OS NOMES DOS ALUNOS
-	//MATRICULADOS.
-	//NÃO É NECESSÁRIO SALVAR DADOS. APENAS LISTAGEM.
-
-//6 - HISTORICO DO ALUNO
-	//SOLICITAR NOME DO ALUNO.
-	//LISTAR TODAS AS DISCIPLINAS MATRICULADAS
-	//(EM ORDEM DE PERIODO)
-	//NÃO É NECESSÁRIO SALVAR DADOS. APENAS LISTAGEM.
-
-
 #define SIZED 50
 
 typedef struct{
@@ -53,12 +18,21 @@ typedef struct{
 	char nome[100];
 	int periodo;
 	int matriculas[50];
+	char alunosMatriculados[50][100];
+	int contAM;
 	int matricula_cont;
 }Disciplina;
 
 typedef struct{
+	char nome[100];
+	int periodo;
+}historicD;
+
+typedef struct{
 	int id;
 	char nome[100];
+	historicD disciplinas[50]; 
+	int cont_diciplina;
 }Aluno;
 
 //datasets
@@ -93,15 +67,19 @@ void matricularAluno(dsAlunos* qtdA, dsDisciplinas* qtdD);
 
 Aluno* buscarAluno(char nome[], dsAlunos* qtd);
 
-void mostrarAluno(Aluno a);
-
-void mostrarDisciplina(Disciplina d);
+void chamada(dsDisciplinas* d);
 
 int alunoRepet(int id, Disciplina* qtd);
 
 void cancelarMatricula(Aluno* a, Disciplina* d);
 
-void excluir(Aluno a);
+void excluir(dsAlunos* a, dsDisciplinas* d);
+
+void ordenarAlunos(Disciplina* d);
+
+void ordenarDisciplina(Aluno* a);
+
+void historico(dsAlunos* a, dsDisciplinas* d);
 
 int main() {
 	
@@ -122,7 +100,20 @@ int main() {
 				matricularAluno(&listaA, &listaD);
 				break;
 			case 4: 
-				cancelarMatricula(&a, &d);
+				excluir(&listaA, &listaD);
+				break;
+			case 5: 
+				chamada(&listaD);
+				break;
+			case 6: 
+				historico(&listaA, &listaD);
+				break;
+			case 0:
+				printf("==========Programa encerrado=========");
+				return 0;
+			default: 
+				printf("Digite uma opção valida!");
+				break;
 		}
 	}while(1);
 }
@@ -154,12 +145,14 @@ Aluno setAluno(){
 	Aluno a;
 	printf("Nome: ");
 	scanf(" %[^\n]", a.nome);
+	a.cont_diciplina = 0;
 	return a;
 }
 
 Disciplina setDisciplina(dsDisciplinas* qtd){
 	Disciplina d;
 	d.matricula_cont = 0;
+	d.contAM = 0;
 	input(STR, "Digite o nome da disciplina: ", d.nome);
 	input(INT, "Digite o periodo da disciplina: ", &d.periodo);
 	return d;
@@ -207,16 +200,26 @@ Disciplina* buscarDisciplina(char nomeD[], dsDisciplinas* qtd){
 	return NULL;
 }
 
-void mostrarAluno(Aluno a){
-	printf("Numero da matricula: %d", a.id);
-	printf("Nome: %s", a.nome);
+void chamada(dsDisciplinas* qtd){
+	char nomeD[100];
+	
+	input(STR, "Digite o nome da Disciplina: ", nomeD);
+	
+	Disciplina* alvoD = buscarDisciplina(nomeD, qtd);
+	
+	if(!alvoD) {
+		printf("Disciplina nao encontrada! Tente novamente... ");
+		next();
+		return;
+	}
+	
+	ordenarAlunos(alvoD);
+	
+	for(int i = 0; i < alvoD->contAM; i++) {
+			printf("Nome: %s\n", alvoD->alunosMatriculados[i]);
+	}
 }
 
-void mostrarDisciplina(Disciplina d){
-	printf("Codigo da disciplina: %d", d.codigo);
-	printf("nome da disciplina: %s", d.nome);
-	printf("Periodo da disciplina: %d", d.periodo);
-}
 
 int alunoRepet(int id, Disciplina* d){
 	for(int i = 0; i < d->matricula_cont; i++) {
@@ -250,7 +253,6 @@ void matricularAluno(dsAlunos* qtdA, dsDisciplinas* qtdD){
 		return;
 	}
 
-	printf("Retorno: %d\n", alunoRepet(alvoA->id, alvoD));
 	if(alunoRepet(alvoA->id, alvoD)){
 		printf("Aluno registrado na mesma diciplina!... Tente novamente \n");
 		next();
@@ -258,31 +260,128 @@ void matricularAluno(dsAlunos* qtdA, dsDisciplinas* qtdD){
 	}
 	
 	
-	alvoD->matriculas[alvoD->matricula_cont] = alvoA->id;	
+	alvoD->matriculas[alvoD->matricula_cont] = alvoA->id;
+	
+	strcpy(alvoD->alunosMatriculados[alvoD->contAM], alvoA->nome);
+	
 	alvoD->matricula_cont++;
+	
+	alvoD->contAM++;
+	strcpy(alvoA->disciplinas[alvoA->cont_diciplina].nome, alvoD->nome);
+	
+	alvoA->disciplinas[alvoA->cont_diciplina].periodo = alvoD->periodo;
+	
+	alvoA->cont_diciplina++;
 	
 }
 
-void excluir(dsAlunos qtdA, dsDisciplinas* qtdD){
+void excluir(dsAlunos* qtdA, dsDisciplinas* qtdD){
+	char nome[100];
+	char nomeD[100];
+	
 	input(STR, "Digite o nome para cancelar a matricula: ", nome);
-	Aluno* alvoA =
-	//termianr
 	
-	Discip
+	Aluno* alvoA = buscarAluno(nome, qtdA);
 	
-	cancelarMatricula(alvo, dis)
+	if(!alvoA){ 
+		printf("Nome do aluno não encontrado! Tente novamente... ");
+		next();
+		return;
+	}
 	
+	input(STR, "Digite a Disciplina que voce quer remove-lo: ", nomeD);
+	
+	Disciplina* alvoD = buscarDisciplina(nomeD, qtdD);
+	
+	if(!alvoD){ 
+		printf("Discipçina nao encontrada! Tente novamente... ");
+		next();
+		return;
+	}
+	
+	printf("matricula cancelada do aluno %s! Pressione enter para continuar... ", alvoA->nome);
+	next();
+	cancelarMatricula(alvoA, alvoD);
+
+}
+
+void ordenarAlunos(Disciplina* d){ 
+	int j;
+	char key[100];
+	for(int i = 0; i < d->contAM; i++) {
+		strcpy(key, d->alunosMatriculados[i]);
+		j = i-1;
+		
+		while( j >= 0 && strcmp(d->alunosMatriculados[j], key) > 0){
+			strcpy(d->alunosMatriculados[j+1], d->alunosMatriculados[j]);
+			j--;
+		}
+		strcpy(d->alunosMatriculados[j+1], key);
+	}
+}
 
 
+void ordenarDisciplina(Aluno* a){
+	int j;
+	historicD key;
+	for(int i = 0; i < a->cont_diciplina; i++) {
+		key = a->disciplinas[i];
+		j = i-1;
+		
+		while( j >= 0 && a->disciplinas[j].periodo > key.periodo) {
+			a->disciplinas[j+1] = a->disciplinas[j];
+			j--;
+		}
+		a->disciplinas[j+1] = key;
+	}
+}
+
+
+void historico(dsAlunos* qtdA, dsDisciplinas* qtdD){
+	char nome[100];
+	
+	input(STR, "Digite o nome para o historico: ", nome);
+	
+	Aluno* alunoH = buscarAluno(nome, qtdA);
+	
+	
+	if(!alunoH) {
+		printf("O nome desse aluno nao existe! Tente novamente...");
+		next();
+		return;
+	}
+	
+	ordenarDisciplina(alunoH);
+	
+	for(int i = 0; i < alunoH->cont_diciplina; i++) {
+		printf("Nome da disciplina: %s", alunoH->disciplinas[i].nome);
+		printf("Periodo: %d\n", alunoH->disciplinas[i].periodo);
+	}
 }
 
 void cancelarMatricula(Aluno *a, Disciplina* d){
+	//remover da disciplina
 	for(int i = 0; i < d->matricula_cont; i++) {
 		if(a->id == d->matriculas[i]) {
 			for(int j = i; j < d->matricula_cont - 1; j++) {
 				d->matriculas[j] = d->matriculas[j+1];
-				d->matricula_cont--;
+				strcpy(d->alunosMatriculados[j], d->alunosMatriculados[j+1]);
 			}
+			
+			d->matricula_cont--;
+			d->contAM--;
+			break;
+		}
+	}
+	//remover do historico
+	for(int i = 0; i < a->cont_diciplina; i++) {
+		if(!strcmp(a->disciplinas[i].nome, d->nome)) {
+			
+			for(int j = i; j < a->cont_diciplina - 1; j++) {
+				a ->disciplinas[j] = a->disciplinas[j+1];
+			}
+			a->cont_diciplina--;
+			break;
 		}
 	}
 }
